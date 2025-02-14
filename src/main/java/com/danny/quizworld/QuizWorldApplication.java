8a59733aa359f8782361d0baffc84c7746fcfbc3
danny <qkrwngur5185@naver.com>
2025-02-14 20:48:33 +0900

    added

---
+++ src/main/java/com/danny/quizworld/QuizWorldApplication.java
@@ -0,0 +1,13 @@
+package com.danny.quizworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizWorldApplication.class, args);
+	}
+
+}
