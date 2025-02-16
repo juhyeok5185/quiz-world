6f1312e6ce6d539ee8a2aea8d25ba0c01f5ed579
danny <qkrwngur5185@naver.com>
2025-02-16 10:26:54 +0900

    added

---
+++ src/main/java/com/danny/quizworld/subject/Subject.java
@@ -0,0 +1,28 @@
+package com.danny.quizworld.subject;
+
+import com.danny.quizworld.common.entity.BaseTimeEntity;
+import com.danny.quizworld.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "t_subject")
+public class Subject extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subject_id")
+    private Long subjectId; // 회원 일련번호
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "name")
+    private String name;
+
+}
