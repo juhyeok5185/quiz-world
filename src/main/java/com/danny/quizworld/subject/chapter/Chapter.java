fe7c6b70f14aa87eb8462fd704a92ef5bd83f746
danny <qkrwngur5185@naver.com>
2025-02-16 10:59:21 +0900

    added

---
+++ src/main/java/com/danny/quizworld/subject/chapter/Chapter.java
@@ -0,0 +1,27 @@
+package com.danny.quizworld.subject.chapter;
+
+import com.danny.quizworld.member.Member;
+import com.danny.quizworld.subject.Subject;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "t_chapter")
+public class Chapter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chapter_id")
+    private Long chapterId; // 회원 일련번호
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_id")
+    private Subject subject;
+
+    @Column(name = "name")
+    private String name;
+}
