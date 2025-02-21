94854cc9c3757b3584eb18e85882ac77ad3f891b
juhyeok5185 <qkrwngur5185@naver.com>
2025-02-21 14:13:17 +0900

    added

--- src/main/java/com/danny/quizworld/question/file/QuestionFileService.java
+++ src/main/java/com/danny/quizworld/question/file/QuestionFileService.java
@@ -1,7 +1,11 @@
 package com.danny.quizworld.question.file;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -10,9 +14,16 @@ public class QuestionFileService {
     private final QuestionFileReader questionFileReader;
     private final QuestionFileStore questionFileStore;
 
-
-    private void save(QuestionFile questionFile){
+    @Transactional
+    public void save(QuestionFile questionFile){
         questionFileStore.save(questionFile);
     }
+
+    public String getSaveFilename(final String filename) {
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        String extension = FilenameUtils.getExtension(filename);
+        return uuid + "." + extension;
+    }
+
 
 }
