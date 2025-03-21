cebb8c400b99920e13d8760108235e312487b31b
juhyeok5185 <qkrwngur5185@naver.com>
2025-03-21 14:43:47 +0900

    subject , chapter 업데이트 , 삭제 테스트 필요

--- src/main/java/com/danny/quizworld/course/study/StudyApiController.java
+++ src/main/java/com/danny/quizworld/course/study/StudyApiController.java
@@ -17,29 +17,29 @@ public class StudyApiController {
     private final CourseFacade courseFacade;
 
 
-    @PostMapping("/{chapterId}/study")
+    @PostMapping("/{chapterId}/studies")
     public ResponseEntity<ApiResponse<StudyResponse>> saveStudy(@PathVariable Long chapterId, @ModelAttribute @Valid StudyCommand.save request) {
         courseFacade.saveStudy(chapterId, request);
         return ResponseEntity.status(201).body(new ApiResponse<>("생성 성공", 201, null));
     }
 
-    @GetMapping("/{chapterId}/study")
+    @GetMapping("/{chapterId}/studies")
     public ResponseEntity<ApiResponse<List<StudyResponse>>> findAllStudyByChapterId(@PathVariable Long chapterId) {
         return ResponseEntity.ok(new ApiResponse<>(courseFacade.findAllStudyByChapterId(chapterId)));
     }
 
-    @GetMapping("/study/{studyId}")
+    @GetMapping("/studies/{studyId}")
     public ResponseEntity<ApiResponse<StudyResponse>> findStudyById(@PathVariable Long studyId) {
         return ResponseEntity.ok(new ApiResponse<>(courseFacade.findStudyById(studyId)));
     }
 
-    @PatchMapping("/study/{studyId}")
+    @PatchMapping("/studies/{studyId}")
     public ResponseEntity<ApiResponse<StudyResponse>> updateStudy(@PathVariable Long studyId , @ModelAttribute @Valid StudyCommand.update request){
         courseFacade.updateStudy(studyId , request);
         return ResponseEntity.ok(new ApiResponse<>("변경 성공", 201, null));
     }
 
-    @DeleteMapping("/study/{studyId}")
+    @DeleteMapping("/studies/{studyId}")
     public ResponseEntity<ApiResponse<StudyResponse>> deleteStudyById(@PathVariable Long studyId) {
         courseFacade.deleteStudyById(studyId);
         return ResponseEntity.status(201).body(new ApiResponse<>("삭제 완료" , 201 ,null));
