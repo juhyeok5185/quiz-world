bddf7f343825d4d38865ad5b5eddb87f3b6bc231
juhyeok5185 <qkrwngur5185@naver.com>
2025-04-17 09:51:41 +0900

    added

--- src/main/java/com/danny/quizworld/common/controller/PageController.java
+++ src/main/java/com/danny/quizworld/common/controller/PageController.java
@@ -1,10 +1,7 @@
 package com.danny.quizworld.common.controller;
 
-import com.danny.quizworld.common.util.Utils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,29 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class PageController {
 
     @GetMapping("/")
-    public String index(Authentication authentication) {
-        if(authentication == null){
-            return "redirect:login";
-        }
-
-        if(Utils.getRole(authentication).equals("ROLE_ADMIN")){
-            return "redirect:admin/main";
-        }
-
-        return "redirect:user/main";
+    public String index() {
+        return "index";
     }
 
     @GetMapping("/{firstUrl}")
-    public String firstUrl(@PathVariable String firstUrl , Authentication authentication) {
-
-        if(firstUrl.equals("login")){
-            if(authentication != null){
-                if(Utils.getRole(authentication).equals("ROLE_ADMIN")){
-                    return "redirect:admin/main";
-                }
-                return "redirect:user/main";
-            }
-        }
+    public String firstUrl(@PathVariable String firstUrl) {
         return firstUrl;
     }
 
@@ -47,15 +27,12 @@ public class PageController {
     }
 
     @GetMapping("/user/{firstUrl}")
-    @PreAuthorize("hasRole('ROLE_USER')")
     public String userFirstUrl(@PathVariable String firstUrl) {
         return "user/" + firstUrl;
     }
 
     @GetMapping("/admin/{firstUrl}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String adminFirstUrl(@PathVariable String firstUrl) {
         return "admin/" + firstUrl;
     }
-
 }
